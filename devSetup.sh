38cdfde6aa1f2d2d332bbb2ebba211e741de156e
Raül Torralba <raul.torralba@artansoft.com>
2015-11-07 00:29:11 +0100

    improved development setup script

--- devSetup.sh
+++ devSetup.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
-if which composer >/dev/null; then
-    echo "Installing dependences..."
-    composer install
+if [[ ! -f .env ]]; then
+    echo -e "\nYou must to create a .env file with a correct enviroment data configured, you have a .env.example to know the configuration structure\n"
+    exit 1
+fi
 
-    echo "Fixing storage folder perms..."
-    chmod -R 775 storage
-
-    echo "Fixing bootstrap/cache folder perms..."
-    chmod -R 775 bootstrap/cache/
-
-    echo "Fixing generating laravel key..."
-    php artisan key:generate
-else
+if [[ $(which composer) == "" ]]; then
     echo -e "\nYou must to be installed composer. Instructions:\n"
     echo -e "curl -sS https://getcomposer.org/installer | php\n"
     echo -e "sudo mv composer.phar /usr/local/bin/composer\n"
+    exit 1
 fi
+
+echo "Installing dependences..."
+composer install
+
+echo "Fixing storage folder perms..."
+chmod -R 775 storage
+
+echo "Fixing bootstrap/cache folder perms..."
+chmod -R 775 bootstrap/cache/
+
+echo "Fixing generating laravel key..."
+php artisan key:generate
